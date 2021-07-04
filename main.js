fb4bcf01f46e16797d10deb14e55c6fa52731a38
jacobsackor <83283604+jacobsackor@users.noreply.github.com>
2021-07-04 20:09:21 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,167 @@
+// No one MESS with this first code it holds the whole project together
+var c = 0;
+function bless(){
+  if(c==0){
+    document.getElementById('list').style.display="inline"; c=1;
+  }
+  else if(c==1) {
+    document.getElementById('list').style.display="none"; c=0;
+
+  }
+}
+//THIS line of codes deals with the images we are workin on. LIst the four images of the county you are work on i need quality images
+var a = 0;
+function magic(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="imrs(1).webp"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="imrs(2).webp"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="executive-mansion-presidential-palace-19601963-monrovia-liberia-20th-picture-id640234657.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="container-ship-registered-liberia-corinthikos-46000561.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function magic2(){
+  if(a==1){
+    document.getElementById('change').src="imrs.webp"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="imrs(1).webp"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="imrs(2).webp"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="executive-mansion-presidential-palace-19601963-monrovia-liberia-20th-picture-id640234657.jpg"; a=3;}
+}
+/*THIS LINE OF CODE DELAS WITH THE SOUTH AFRICA PAGE*/
+var a = 0;
+function southafrica(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="matt-halls-7kYfhku6X2w-unsplash.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="summertime.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="walking.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="south africa/kyle-philip-coulson-PoRBsATX8Es-unsplash.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function southafrica2(){
+  if(a==1){
+    document.getElementById('change').src="south africa/mpumelelo-macu-l_YNobbDYJk-unsplash.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="south africa/matt-halls-7kYfhku6X2w-unsplash.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="summertime.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="walking.jpg"; a=3;}
+}
+
+/*THIS LINE OF CODE DELAS WITH THE NIGERIA PAGE*/
+var a = 0;
+function nigeria(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="IMG-20210622-WA0023.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="IMG-20210622-WA0019.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="IMG-20210622-WA0018.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="IMG-20210622-WA0025.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function nigeria2(){
+  if(a==1){
+    document.getElementById('change').src="IMG-20210622-WA0028.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="IMG-20210622-WA0023.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="IMG-20210622-WA0019.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="IMG-20210622-WA0018.jpg"; a=3;}
+}
+
+
+/*THIS LINE OF CODE DELAS WITH THE Sierra Leone PAGE*/
+var a = 0;
+function sierra(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="sierra_leone_0.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="n1pansrfj1t01.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="panorama-view-freetown-sierra-leone-260nw-1780663949.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="istockphoto-174703540-612x612.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function sierra2(){
+  if(a==1){
+    document.getElementById('change').src="freetown.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="sierra_leone_0.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="n1pansrfj1t01.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="panorama-view-freetown-sierra-leone-260nw-1780663949.jpg"; a=3;}
+}
+
+
+/*THIS LINE OF CODE DELAS WITH THE Ghana PAGE*/
+var a = 0;
+function ghana(){
+  //THE FIRST IMAGE GOES HERE
+  if(a==0){
+    document.getElementById('change').src="depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=1;
+  }
+  else if(a==1) {
+    //THE SECOND IMAGE GOES HERE
+    document.getElementById('change').src="Ghana1.jpg"; a=2;}
+    else if(a==2) {
+      //THE THIRD IMAGE GOES HERE
+      document.getElementById('change').src="girls-dancing-at-an-official-public-ceremony-in-ho-ghana-africa-AG812X.jpg"; a=3;}
+      else if(a==3) {
+        //THE LAST IMAGE GOES HERE
+        document.getElementById('change').src="Ghana2.jpg";a=4;}
+}
+//THIS SECOND LINE OF CODE IS DIFFICULT SO PAY ATTENTION
+var a = 0;
+function ghana2(){
+  if(a==1){
+    document.getElementById('change').src="depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=0;
+  }
+  else if(a==2) {
+    document.getElementById('change').src="depositphotos_1093316-stock-photo-boats-on-the-beach-on.jpg"; a=1;}
+    else if(a==3) {
+      document.getElementById('change').src="istockphoto-108203368-612x612.jpg"; a=2;}
+      else if(a==4) {
+        document.getElementById('change').src="girls-dancing-at-an-official-public-ceremony-in-ho-ghana-africa-AG812X.jpg"; a=3;}
+}
+
